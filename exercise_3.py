5783b76b758518e8442919302afa9e9f760ff391
viankahmercado <angelviankahmercado@gmail.com>
2024-01-26 02:09:27 +0800

    description

---
+++ exercise_3.py
@@ -0,0 +1,10 @@
+# Assignment 5:
+# Do the exercise 1-15 in https://pynative.com/python-basic-exercise-for-beginners/
+# Try do challenge yourself by not checking the "solution"
+
+# Exercise 3: 
+# Print characters from a string that are present at an even index number
+# Write a program to accept a string from the user and display characters that are present at an even index number.
+
+# For example, str = "pynative" so you should display ‘p’, ‘n’, ‘t’, ‘v’.
+
